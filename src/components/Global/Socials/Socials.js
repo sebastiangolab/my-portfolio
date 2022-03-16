e252b851fffc4c6dce2bc9b740bc00d84070dae4
sebastiangolab <sebagolab97@gmail.com>
2022-03-16 20:14:02 +0100

    Add social links

--- src/components/Global/Socials/Socials.js
+++ src/components/Global/Socials/Socials.js
@@ -11,16 +11,16 @@ const Socials = (props) => (
     <div>
       <p>You can find me on</p>
       <Row>
-        <a href="#">
+        <a href="https://www.linkedin.com/in/sebastian-go%C5%82%C4%85b/">
           <FaLinkedinIn />
         </a>
-        <a href="#">
+        <a href="https://github.com/sebastiangolab">
           <FaGithubAlt />
         </a>
-        <a href="#">
+        <a href="https://www.instagram.com/seebastiangolab/">
           <FaInstagram />
         </a>
-        <a href="#">
+        <a href="https://www.facebook.com/golomp1997/">
           <FaFacebookF />
         </a>
       </Row>
